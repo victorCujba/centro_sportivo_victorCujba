7dd2bce53530afa86dbec91adf06a29488141d06
cujbaVictor <cujba.victor96@gmail.com>
2023-10-02 17:56:42 +0200

    initial commit base project generated

---
+++ centro-sportivo-cv/src/main/java/it/euris/javaacademy/centrosportivocv/CentroSportivoCvApplication.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.centrosportivocv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroSportivoCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentroSportivoCvApplication.class, args);
+	}
+
+}
---
+++ centro-sportivo-cv/src/test/java/it/euris/javaacademy/centrosportivocv/CentroSportivoCvApplicationTests.java
@@ -0,0 +1,13 @@
+package it.euris.javaacademy.centrosportivocv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroSportivoCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
